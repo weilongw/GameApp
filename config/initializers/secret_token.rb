512c456f9b0385c24c1d145f13ead0e271bdda5c
weilongw <weilongw@andrew.cmu.edu>
2012-09-17 17:19:56 -0400

    first blood

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GameApp::Application.config.secret_token = 'f928767ffa825c4e7cd51ec66e226ff30c111cdf1e02273298aed35d8cea1181266e4db3831efe2d38e1693b2ef9f2229e32eed3f860484bf0ece6384b0b8ebd'
