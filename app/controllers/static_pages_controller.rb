9a39d4a4646c335f3c95eb6c15039bfd708f75c1
weilongw <weilongw@andrew.cmu.edu>
2012-09-17 22:50:13 -0400

    finish static

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,13 @@
 class StaticPagesController < ApplicationController
   def home
+    @winfo = "home"
   end
 
   def help
   end
+
+  def about
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ GameApp::Application.routes.draw do
 
   get "static_pages/help"
 
+  get "static_pages/about"
   resources :games
 
   resources :users
