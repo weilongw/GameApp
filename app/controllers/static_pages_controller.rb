6722eaf7ba15c0b08249f279cf7f7ba78af5016e
Qing Shen <sqf3d@163.com>
2012-09-18 15:55:42 -0400

    router

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,12 +1,14 @@
 class StaticPagesController < ApplicationController
   def home
-    @winfo = "home"
   end
 
   def help
   end
 
   def about
+  end
+
+  def contact
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,14 @@ GameApp::Application.routes.draw do
   get "static_pages/help"
 
   get "static_pages/about"
+
+  get "static_pages/contact"
+
+
+  match '/help', :to=> 'static_pages#help'
+  match '/about', :to => 'static_pages#about'
+  match '/contact', :to=> 'static_pages#contact'
+  match '/', :to=> 'static_pages#home'
   resources :games
 
   resources :users
@@ -57,7 +65,7 @@ GameApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index_backup.html.
-  # root :to => 'welcome#index'
+  root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
