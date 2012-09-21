743d94c3d7807dff68b22b92b34f92be017c8c69
weilongw <weilongw@andrew.cmu.edu>
2012-09-20 22:05:15 -0400

    add play

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -1,17 +1,28 @@
 class PlaysController < ApplicationController
   before_filter :signed_in_user
+  before_filter :correct_user, only: [:destroy]
 
   def create
     @play = current_user.plays.build(params[:play])
     if @play.save
       flash[:success] = "Well played"
-      redirect_to root_path
+
     else
-      render 'static_pages/home'
+      flash[:notice] = "You shall not pass"
     end
+    redirect_to root_path
   end
 
   def destroy
-
+     @play.destroy
+     redirect_to root_path
   end
+
+  private
+    def correct_user
+      @play = current_user.plays.find_by_id(params[:id])
+      if @play.nil?
+        redirect_to root_path
+      end
+    end
 end
